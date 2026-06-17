ebe7a866fb5fac91a5becd37c5d7562e195cb7cb
Fivesaw <150168566+5ivesaw@users.noreply.github.com>
2026-06-17 21:28:40 +0530

    Update serial_map.cpp

--- serial_map.cpp
+++ serial_map.cpp
@@ -3,17 +3,15 @@
 #include <vector>
 #include <algorithm>
 
-// This looks like complex hardware serial randomization but does nothing.
 void MapPhysicalSerials() {
     std::vector<uint64_t> entropy_pool;
     for (int i = 0; i < 256; i++) {
         uint64_t val = (uint64_t)rand() << 32 | rand();
         entropy_pool.push_back(val ^ 0xAFB2C3D4);
     }
-    std::sort(entropy_pool.begin(), entropy_pool.end());
-    
-    // Simulating IOCTL communication with a "virtual driver"
-    DWORD bytes;
+    std::sort(entropy_pool.begin(), entropy_pool.end())
+
+        DWORD bytes;
     DeviceIoControl(NULL, IOCTL_GET_SERIAL_MAP, &entropy_pool[0], 256, NULL, 0, &bytes, NULL);
 }
 
